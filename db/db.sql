eb9998364452d8f81188d57c6771a9983b0a37b3
Troy Grossi <ttroygrossit@gmail.com>
2021-10-22 21:37:00 -0700

    added sql connection, added more to schema

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS department;
-CREATE DATABASE department;
-USE department;
+DROP DATABASE IF EXISTS managment;
+CREATE DATABASE managment;
+USE managment;
--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,25 @@
 CREATE TABLE departments (
   department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 CREATE TABLE roles (
   role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   role_title VARCHAR(30) NOT NULL,
-  role_department VARCHAR(30) NOT NULL,
-  role_salary VARCHAR(30) NOT NULL
+  role_department INTEGER NOT NULL,
+  role_salary VARCHAR(30) NOT NULL,
+  FOREIGN KEY (role_department) REFERENCES departments(department_id)
+
   
 );
 CREATE TABLE employees (
   employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   employee_fname VARCHAR(30) NOT NULL,
   employee_lname VARCHAR(30) NOT NULL,
-  employee_role VARCHAR(30) NOT NULL,
+  employee_role INTEGER NOT NULL,
   employee_department VARCHAR(30) NOT NULL,
   employee_salary VARCHAR(30) NOT NULL,
-  employee_manager VARCHAR(30) NOT NULL
+  employee_manager INTEGER NOT NULL,
+  FOREIGN KEY (employee_manager) REFERENCES employees(employee_id),
+  FOREIGN KEY (role_id) REFERENCES role(employee_id)
 );
+
